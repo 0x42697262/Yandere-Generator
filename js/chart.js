5599899b4da04f4ef318e6978ddac085584e0963
KrulYuno <giogulane.wineffgulane@gmail.com>
2019-06-02 21:01:49 +0900

    Still making...

---
+++ js/chart.js
@@ -0,0 +1,39 @@
+var age = ["Your yandere is younger than you, by 5-10 years. Onii-chan!",
+"You're the same age. How convenient! (And likely.)", "Your yandere is older than you by 5 to 10 years. Ara ara~."];
+var skin = ["Very dark/black", "Deeply tanned","Tanned", "Light", "Very light/pale"];
+var eyes = ["Brown or Black", "Blue", "Red", "Yellow", "Purple", "Green", "Shifting colors"];
+var hair = ["Black", "Brown", "Blonde", "Red", "Green", "Purple", "Blue", "Blend of two colors of your choice."];
+var breasts = ["She's got nothing there, flat as board. She's an A cup at best.",
+"Her breasts are small, but very sensitive. B or C cup.", "Her breasts are average, at about a D cup. Great for cupping!",
+"Her tits are huge. She's got an E-cup, and she might have a back problem!",
+"She has huge knockers! She's got an F or G cup, depending on your tastes."];
+var ass = ["The flattest. Don't want none unless you got buns, hun!",
+"Getting there. It's not much to look at, but a good flex would improve it.",
+"Her ass is average. Good enough for most.", "Aw shit. Now you can take your hand and squeeze that shit!",
+"DAMN! You like big butts and you cannot lie, she definitely got one!"];
+var background = ["Your yandere is supernatural. She might be a spirit, or a monster of some kind. She might even be some of monster girl.",
+"Your yandere is extremely rich. Her parents might own a large corporation. They own a private island, and she has an army of private servants at her beck and call. She has money, and she's willing to use it.",
+"Your yandere is the daughter of a politician. She might not be fabulously wealthy, but she's well off. More importantly, she has connections that she can call upon that might get certain things... ignored.",
+"Your yandere is a loner. She doesn't talk to other people much, and she avoids them as much as she can, apart from you of course.",
+"Your yandere is an orphan. She had to live on the street to survive, learning to scavenge food for herself and live under the hatred of others. The experience may have scarred her.",
+"Your yandere is completely normal. Or just maybe, she's good at keeping up appearances. Nobody will suspect a thing from her.",
+"Your yandere is a childhood friend of yours, and she knows you and your family intimately. Your family won't see anything strange about her creeping around your house, and will give her all the keys she needs to get anywhere.",
+"Your yandere is a foreigner. She comes from another country. She speaks its languages and practices its customs. She's got more worldly experience than you, and she has contacts in other countries that she can use.",
+"Your yandere is poor. She probably comes from a family of seven, where she was neglected due to her parents not having enough time for her. Even so, she has that family, and she can use it against you.",
+"Your yandere is a magical girl. Somehow, she got ahold of a contract of some sort, and now the can use magic to her advantage. Her patron may have an unknown objective that involves your death."];
+var personality = ["She is innocent. She doesn't know much about the world.",
+"She is a doormat. She will do anything you tell her... to a point.",
+"She is an airhead. She has her own incomprehensible rhyme and reason.",
+"She is calm and cool. She will remain stoic and cold until she snaps.",
+"She is tsundere. It's not like I want to kill you, baka!",
+"She is active and energetic. She has lots of willpower and personal power.",
+"She is shy. She won't talk much, and you don't want to hear it anyway.",
+"She is proper and courteous. She follows all procedures and plans to a fault.",
+"She is a slut. She craves the dick, especially yours.",
+"She is dark and sad. You'd say she had depression, didn't know the truth."];
+var int_ath = ["Your yandere is extremely intelligent. She learns new things quickly, and she has a wealth of knowledge already built up.",
+"Average: She doesn't appear smart or strong. But she's hiding something.",
+"Your yandere is very athletic. She may be on the track and field team, or participate in other sports."];
+var sub_int_ath1 = "She's a genius. She's likely already won awards for her intellect, and you won't be able to outsmart her.";
+var sub_int_ath2 = ["She's hiding her keen intellect from you through a veneer of idiocy.", "She's hiding her strength and speed from you until a critical moment"];
+var sub_int_ath3 = "Your yandere is so fit chat she is Olympic level. You won't be able to outrun her.";
